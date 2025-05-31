921a1785ea8938782015c6e0553c6fe9d7c1f2eb
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2025-05-31 09:27:00 +0700

    Create Contact

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import UserLogin from "./components/User/UserLogin.jsx";
 import DashboardLayout from "./components/DashboardLayout.jsx";
 import UserProfile from "./components/User/UserProfile.jsx";
 import UserLogout from "./components/User/UserLogout.jsx";
+import CreateContact from "./components/Contact/CreateContact.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -17,9 +18,16 @@ createRoot(document.getElementById("root")).render(
           <Route path="/login" element={<UserLogin />}></Route>
         </Route>
         <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route path="contacts" element={<div>Contacts</div>}></Route>
-          <Route path="users/profile" element={<UserProfile />}></Route>
-          <Route path="users/logout" element={<UserLogout />}></Route>
+          <Route path="users">
+            <Route path="profile" element={<UserProfile />}></Route>
+            <Route path="logout" element={<UserLogout />}></Route>
+          </Route>
+
+          <Route path="contacts">
+            {/* index default halaman */}
+            <Route index element={<div>Contacts</div>}></Route>
+            <Route path="create" element={<CreateContact />}></Route>
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
